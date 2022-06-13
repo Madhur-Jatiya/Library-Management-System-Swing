baca825f64c0542bbd0015dc31ea1ed43eee0d47
Madhurmoms <madhurjatiya13@gmail.com>
2022-06-13 07:40:01 +0530

    first commit

---
+++ src/library_managment_system/Library_managment_system.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package library_managment_system;
+
+/**
+ *
+ * @author 91940
+ */
+public class Library_managment_system {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Home_page home_page = new Home_page();
+        home_page.main(args);
+    }
+    
+}
